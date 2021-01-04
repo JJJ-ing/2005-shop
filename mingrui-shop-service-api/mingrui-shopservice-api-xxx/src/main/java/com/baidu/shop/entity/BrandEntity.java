c86ec9b6f14d06449454d86040fd4a9ceff6ec9a
JJJ-ing <JJJ_ing@163.com>
2021-01-04 19:00:38 +0800

    aaa

--- mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/entity/BrandEntity.java
+++ mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/entity/BrandEntity.java
@@ -21,7 +21,7 @@ public class BrandEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    //品牌名称
+
     private String name;
 
     private String image;
